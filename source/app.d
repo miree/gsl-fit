9b1d44b2aac392f3f711aa03460a854f42f1f1a9
miree <michaelate@gmail.com>
2014-06-13 20:45:33 +0200

    Update app.d

--- source/app.d
+++ source/app.d
@@ -14,7 +14,7 @@
 //You should have received a copy of the GNU General Public License
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
-//comments & questions: michaelate@gmail.com
+//comments & questions: michaelate at gmail.com
 
 import pegged.grammar;
 
